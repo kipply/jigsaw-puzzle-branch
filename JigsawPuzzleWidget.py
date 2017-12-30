3c302ea540fb7a2cb7dce9ec36b0f6b5594bc52b
Carol Chen <carolyfisher@gmail.com>
2017-12-30 11:44:55 -0500

    n

--- JigsawPuzzleWidget.py
+++ JigsawPuzzleWidget.py
@@ -143,6 +143,7 @@ class JigsawPiece (Gtk.EventBox):
             # Won't work as cairo.Region is not available in Python 2
             # mregion = Gdk.cairo_region_create_from_surface(self.shape)
             # self.get_window().shape_combine_region(mregion, 0, 0)
+            self.get_window().ensure_native()
 
 class CutterBasic (object):
     """ Cutters are used to create the connectors between pieces.
