9785fe004369713e0ab5be6b1d2815aac0df4678
Carol Chen <carolyfisher@gmail.com>
2017-12-30 16:30:20 -0500

    n

--- JigsawPuzzleWidget.py
+++ JigsawPuzzleWidget.py
@@ -150,7 +150,7 @@ class JigsawPiece (Gtk.EventBox):
             logging.error("W " + str(self.shape.get_width()))
             self.get_window().set_background_rgba(255, 255, 255, 0)
             self.get_window().scroll(self.x, self.y)
-            self.get_window().move(self.shape.get_height(), self.shape.get_width())
+            # self.get_window().move(self.shape.get_height(), self.shape.get_width())
 
 class CutterBasic (object):
     """ Cutters are used to create the connectors between pieces.
