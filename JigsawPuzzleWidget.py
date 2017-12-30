a077d45458c12400d281a95c78a194856f65ef35
Carol Chen <carolyfisher@gmail.com>
2017-12-30 11:58:31 -0500

    n

--- JigsawPuzzleWidget.py
+++ JigsawPuzzleWidget.py
@@ -141,10 +141,9 @@ class JigsawPiece (Gtk.EventBox):
         if self.shape is not None:
             # Won't work as cairo.Region is not available in Python 2
             self.get_window().ensure_native()
-            logging.error("nate?"+ str(self.get_window().ensure_native()))
             logging.error(self.get_window().is_shaped())
-            # mregion = Gdk.cairo_region_create_from_surface(self.shape)
-            # self.get_window().shape_combine_region(mregion, 0, 0)
+            mregion = Gdk.cairo_region_create_from_surface(self.shape)
+            self.get_window().shape_combine_region(mregion, 0, 0)
             logging.error(self.get_window().is_shaped())
 
 class CutterBasic (object):
