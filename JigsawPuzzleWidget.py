37600d171ac6f80cd006543bd22c60d99925ced3
Carol Chen <carolyfisher@gmail.com>
2017-12-30 12:06:06 -0500

    n

--- JigsawPuzzleWidget.py
+++ JigsawPuzzleWidget.py
@@ -142,12 +142,7 @@ class JigsawPiece (Gtk.EventBox):
             # Won't work as cairo.Region is not available in Python 2
             self.get_window().ensure_native()
             logging.error(self.get_window().is_shaped())
-            # try:
-            #     mregion = Gdk.cairo_region_create_from_surface(self.shape)
-            #     self.get_window().shape_combine_region(mregion, 0, 0)
-            #     logging.error(self.get_window().is_shaped())
-            # except:
-            #     self.get_window().resize(100, 100)
+            self.get_window().resize(100, 100)
 
 
 class CutterBasic (object):
